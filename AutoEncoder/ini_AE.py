15dfefb2083b0f9686fddae86df5952c6819bca6
Piotrowski Florian <79841404+Fpiotro@users.noreply.github.com>
2021-11-16 15:09:37 +0100

    Update ini_AE.py

--- AutoEncoder/ini_AE.py
+++ AutoEncoder/ini_AE.py
@@ -13,16 +13,15 @@ INI_PATH = BASE_PATH.joinpath("ini").resolve()
 # ====================================================
 # Ini
 # ===================================================
-config = ConfigParser()
-config['Preprocessing_parameters'] = {}
-config['Preprocessing_parameters']['size_image'] = '224'
-
-with open(INI_PATH.joinpath("preprocessing.ini"), 'w') as configfile:
-    config.write(configfile)
-
 config = ConfigParser(allow_no_value=True)
+
 config['Data_parameters'] = {}
 config['Data_parameters']['data_name'] = 'AE_2021'
+config['Data_parameters']['data_path_input'] = ''
+config['Data_parameters']['data_path_output'] = ''
+
+config['Preprocessing_parameters'] = {}
+config['Preprocessing_parameters']['size_image'] = '224'
 
 config['Model_parameters'] = {}
 config['Model_parameters']['n_channels'] = '1'
@@ -39,14 +38,10 @@ config['Training_parameters']['print_freq'] = '1'
 config['Training_parameters']['checkpoint'] = 'False'
 config['Training_parameters']['checkpoint_path'] = './checkpoint/checkpoint_AE_2021.pth.tar'
 
-with open(INI_PATH.joinpath("params.ini"), 'w') as configfile:
-    config.write(configfile)
-
-config = ConfigParser()
 config['Main_parameters'] = {}
 config['Main_parameters']['debug'] = 'False'
 config['Main_parameters']['random_seed'] = '42'
 config['Main_parameters']['batch_size'] = '32'
 
-with open(INI_PATH.joinpath("main.ini"), 'w') as configfile:
+with open(INI_PATH.joinpath("params.ini"), 'w') as configfile:
     config.write(configfile)
